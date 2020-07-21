66763dd2cc12328622003490b7a0a01d5f7b5a21
alan-lam <alanlam00@gmail.com>
2020-07-21 13:22:09 -0700

    upload TileVania

---
+++ TileVania/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = 0.2f;
+        yield return new WaitForSecondsRealtime(2f);
+        Time.timeScale = 1f;
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
